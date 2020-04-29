ff19ee55686b2bddf38a91068c6791ab195ec767
Gantushig Bayasgalan <gantushig21@gmail.com>
2020-04-29 15:43:53 -0500

    fixed little bug

--- w1d4/js/jquery.js
+++ w1d4/js/jquery.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#form2").submit(e => {
         e.preventDefault();
         $("div.result").empty();
-        $("div.form-group input").each((id, e) => {
+        $("div.form-group input, select").each((id, e) => {
             $("div.result").append($("<p>", {
                 text: $(e).val()
             }));
